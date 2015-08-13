cf14310dc2ecc1addeb5bd6022faba1f9531f37a
Ercan Ozkaya <ercan@timble.net>
2015-08-13 18:20:00 +0300

    Update for Joomlatools framework 3.0

--- README.md
+++ README.md
@@ -7,19 +7,17 @@ This Joomla plugin allows users to login using basic HTTP authentication.
 Go to the root directory of your Joomla installation in command line and execute this command:
 
 ```
-composer require nooku/joomla-basicauth:*
+composer require joomlatools/joomla-basicauth:~2.0
 ```
 
 ## Usage
 
 After installing the plugin, publish it in the Plugin manager. 
 
-Any menu item with access level set to _Registered_ or _Special_ will now prompt the user to login using basic HTTP auth if he/she tries to open this page directly. 
-
 ## Requirements
 
 * Composer
-* Joomla version 2.5 and up.
+* Joomla version 3.4 and up.
 
 ## Contributing
 
--- basicauth.php
+++ basicauth.php
@@ -2,12 +2,10 @@
 /**
  * Basic HTTP authentication for Joomla - https://github.com/joomlatools/joomla-basicauth
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @copyright	Copyright (C) 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link		https://github.com/joomlatools/joomla-basicauth for the canonical source repository
  */
-
-defined( '_JEXEC' ) or die( 'Restricted access' );
 
 /**
  * Basic HTTP auth plugin
@@ -19,88 +17,21 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 class plgSystemBasicAuth extends JPlugin
 {
     /**
-     * Constructor.
+     * Log user in from the basic authentication credentials in the request if possible
      *
-     * @param   object  &$subject  The object to observe
-     * @param   array   $config    An array that holds the plugin configuration
+     * onAfterInitialise is used here to make sure that Joomla doesn't display error messages for menu items
+     * with registered and above access levels.
      */
-    public function __construct($subject, $config = array())
+    public function onAfterInitialise()
     {
-        $input = JFactory::getApplication()->input;
-
-        // See if the client has sent authorization headers
-        if(strpos(PHP_SAPI, 'cgi') !== false) {
-            $authorization = $input->server->get('REDIRECT_HTTP_AUTHORIZATION', null, 'string');
-        } else {
-            $authorization = $input->server->get('HTTP_AUTHORIZATION', null, 'string');
-        }
-
-        // If basic authorization is available, store the username and password in the $_SERVER globals
-        if(strstr($authorization, 'Basic'))
-        {
-            $parts = explode(':', base64_decode(substr($authorization, 6)));
-
-            if(count($parts) == 2)
-            {
-                $input->server->set('PHP_AUTH_USER', $parts[0]);
-                $input->server->set('PHP_AUTH_PW', $parts[1]);
-            }
-        }
-
-        parent::__construct($subject, $config);
-    }
-
-    /**
-     * Ask for authentication and log the user in into the application.
-     *
-     * @return  void
-     */
-    public function onAfterRoute()
-    {
-        $app = JFactory::getApplication();
-
-        $username = $app->input->server->get('PHP_AUTH_USER', null, 'string');
-        $password = $app->input->server->get('PHP_AUTH_PW', null, 'string');
-
-        if ($username && $password)
-        {
-            if (!$this->_login($username, $password, $app)) {
-                throw new Exception('Login failed', 401);
-            }
-        }
-    }
-
-    /**
-     * Logs in a given user to an application.
-     *
-     * @param string $username    The username.
-     * @param string $password    The password.
-     * @param object $application The application.
-     *
-     * @return bool True if login was successful, false otherwise.
-     */
-    protected function _login($username, $password, $application)
-    {
-        // If we did receive the user credentials from the user, try to login
-        if($application->login(array('username' => $username, 'password' => $password)) !== true) {
-            return false;
-        }
-
-        // If we have logged in succesfully, make sure to fullfil
-        // Koowa's CSRF authenticator checks if the framework is loaded.
-        if (class_exists('Koowa'))
+        if (class_exists('Koowa') && JFactory::getUser()->guest)
         {
             $manager = KObjectManager::getInstance();
+            $basic   = $manager->getObject('com:koowa.dispatcher.authenticator.basic');
 
-            $request = $manager->getInstance()->getObject('com:koowa.dispatcher.request');
-            $user    = $manager->getInstance()->getObject('user');
-            $token   = $user->getSession()->getToken();
-
-            $request->setReferrer(JUri::root());
-            $request->getHeaders()->add(array('X-Xsrf-Token' => $token));
-            $request->getCookies()->add(array('csrf_token' => $token));
+            if ($basic->getUsername()) {
+                $basic->authenticateRequest($manager->getObject('com:koowa.dispatcher')->getContext());
+            }
         }
-
-        return true;
     }
 }
